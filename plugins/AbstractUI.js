fb2925c2e1825611c162aabd4029b993b2517415
管晓敏 <guanxiaomin@example.com>
2020-03-10 15:41:53 +0800

    基础uijs更新

---
+++ plugins/AbstractUI.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * @Author: gxm
+ * @Date: 2020-03-06 13:53:28
+ * @Last Modified by: gxm
+ * @Last Modified time: 2020-03-09 18:57:15
+ */
+exports.__esModule = true;
+var UIType;
+(function (UIType) {
+    UIType[UIType["permanent"] = 0] = "permanent";
+    UIType[UIType["Normal"] = 1] = "Normal";
+    UIType[UIType["Pop"] = 2] = "Pop";
+    UIType[UIType["Tips"] = 3] = "Tips";
+    UIType[UIType["Monopoly"] = 4] = "Monopoly";
+    UIType[UIType["actitity"] = 5] = "actitity";
+})(UIType = exports.UIType || (exports.UIType = {}));
+var OrientationType;
+(function (OrientationType) {
+    OrientationType[OrientationType["hroizontal"] = 0] = "hroizontal";
+    OrientationType[OrientationType["vertical"] = 1] = "vertical";
+})(OrientationType = exports.OrientationType || (exports.OrientationType = {}));
