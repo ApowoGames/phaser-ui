de68afe015af79e5eed353bbcd0168c1b6969c75
guanxiaomin <guanxiaomin@apowo.com>
2020-04-17 17:38:59 +0800

    update code

--- lib/ui/interface/panel/IAbstractPanel.ts
+++ lib/ui/interface/panel/IAbstractPanel.ts
@@ -4,10 +4,9 @@ export interface IAbstractPanel extends AbstractUI {
     isShow(): boolean;
     setFollow(gameObject: any, fromScene: Phaser.Scene, callBack?: Function);
     tweenExpand(show: boolean);
-    resize(wid: number, hei: number);
+    resize(wid?: number, hei?: number);
     show(param?: any);
     update(param: any);
     hide();
     destroy();
 }
-
