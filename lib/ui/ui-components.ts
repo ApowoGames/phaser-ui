8357393c0de40ace953a11a42ae957a06200579d
guanxiaomin <guanxiaomin@apowo.com>
2020-06-18 10:26:01 +0800

    update to dev

--- lib/ui/ui-components.ts
+++ lib/ui/ui-components.ts
@@ -1,6 +1,6 @@
 import "tooqinggamephaser";
-import RoundRectangle from "./roundrectangle/RoundRectangle.js";
-import BBCodeText from "./bbcodetext/BBCodeText.js";
+// import RoundRectangle from "./roundrectangle/RoundRectangle.js";
+// import BBCodeText from "./bbcodetext/BBCodeText.js";
 import TagText from "./tagtext/TagText.js";
 import Container from "./container/Container.js";
 import Canvas from "./canvas/Canvas.js";
@@ -28,49 +28,49 @@ import IsPointerInBounds from "../plugins/utils/input/IsPointerInBounds.js";
 import { Show, Hide, IsShown } from "./utils/Hide.js";
 import Edit from "../plugins/behaviors/textedit/Edit.js";
 import { WaitEvent, WaitComplete } from "./utils/WaitEvent.js";
-import { BaseUI } from "./baseUI/BaseUI.js";
-import { BaseMediator } from "./baseUI/BaseMediator.js";
-import { Button } from "./button/Button.js";
-import { NineSliceButton } from "./button/NineSliceButton.js";
-import { ComboBox } from "./combobox/Combobox.js";
-import { GameGridTable } from "./gridtable/GameGridTable.js";
-import { NinePatchSkin } from "./interface/ninepatch/NinePatchSkin.js";
-import { MessageBox } from "./messageBox/MessageBox.js";
-import { NinePatch } from "./ninepatch/NinePatch.js";
-import { NineSlicePatch } from "./ninepatch/NineSlicePatch.js";
-import { Panel } from "./panel/Panel.js";
-import { ProgressBar } from "./progressbar/ProgressBar.js";
-import { GameScroller } from "./scroller/GameScroller.js";
-import { BaseScroller } from "./scroller/Scroller.js";
-import { NinePatchTabButton } from "./tab/NinePatchTabButton.js";
-import { TabButton } from "./tab/TabButton.js";
+// import { BaseUI } from "./baseUI/BaseUI.js";
+// import { BaseMediator } from "./baseUI/BaseMediator.js";
+// import { Button } from "./button/Button.js";
+// import { NineSliceButton } from "./button/NineSliceButton.js";
+// import { ComboBox } from "./combobox/Combobox.js";
+// import { GameGridTable } from "./gridtable/GameGridTable.js";
+// import { NinePatchSkin } from "./interface/ninepatch/NinePatchSkin.js";
+// import { MessageBox } from "./messageBox/MessageBox.js";
+// import { NinePatch } from "./ninepatch/NinePatch.js";
+// import { NineSlicePatch } from "./ninepatch/NineSlicePatch.js";
+// import { Panel } from "./panel/Panel.js";
+// import { ProgressBar } from "./progressbar/ProgressBar.js";
+// import { GameScroller } from "./scroller/GameScroller.js";
+// import { BaseScroller } from "./scroller/Scroller.js";
+// import { NinePatchTabButton } from "./tab/NinePatchTabButton.js";
+// import { TabButton } from "./tab/TabButton.js";
 /**
  * @namespace tooqingui
  */
 const tooqingui = {
-  RoundRectangle,
-  BBCodeText,
+  // RoundRectangle,
+  // BBCodeText,
   TagText,
   Container,
   Canvas,
   Sizer,
   GridSizer,
-  BaseUI,
-  BaseMediator,
-  Button,
-  NineSliceButton,
-  ComboBox,
-  GameGridTable,
-  NinePatchSkin,
-  MessageBox,
-  NinePatch,
-  NineSlicePatch,
-  Panel,
-  ProgressBar,
-  GameScroller,
-  BaseScroller,
-  NinePatchTabButton,
-  TabButton,
+  // BaseUI,
+  // BaseMediator,
+  // Button,
+  // NineSliceButton,
+  // ComboBox,
+  // GameGridTable,
+  // NinePatchSkin,
+  // MessageBox,
+  // NinePatch,
+  // NineSlicePatch,
+  // Panel,
+  // ProgressBar,
+  // GameScroller,
+  // BaseScroller,
+  // NinePatchTabButton,
+  // TabButton,
 
 
   Label,
